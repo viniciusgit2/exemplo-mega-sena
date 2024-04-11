68341424d7dbccaf58c4f4dfb0973d6161e499a1
viniciusgit2 <4xv6patelfin@gmail.com>
2024-04-11 10:50:28 -0300

    feito

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Link from "next/link";
+import Mega from '../pages/Mega'
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+   <div>
+<Link href='Mega'>megasena</Link>
+      </div>
+    
+  )
+}
